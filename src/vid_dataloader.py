c4cfaade11746023640e0f51dea291613f2efa12
Monjurul <raju.monjurulkarim@gmail.com>
2022-01-16 16:36:11 -0500

    video frame sequence corrected

--- src/vid_dataloader.py
+++ src/vid_dataloader.py
@@ -8,16 +8,18 @@ import numpy as np
 
 from PIL import Image
 
-
+os.environ['CUDA_VISIBLE_DEVICES']= '0'
 
 class MySampler(torch.utils.data.Sampler):
     def __init__(self, end_idx, seq_length):
         indices = []
         for i in range(len(end_idx)-1):
             start = end_idx[i]
+            # end = end_idx[i+1] - seq_length
             end = end_idx[i+1] - seq_length
             indices.append(torch.arange(start, end))
         indices = torch.cat(indices)
+
         self.indices = indices
 
     def __iter__(self):
@@ -29,11 +31,12 @@ class MySampler(torch.utils.data.Sampler):
 
 
 class MyDataset(Dataset):
-    def __init__(self, image_paths, seq_length, transform, length):
+    def __init__(self, image_paths, seq_length, transform, length, device=("cuda" if torch.cuda.is_available() else "cpu")):
         self.image_paths = image_paths
         self.seq_length = seq_length
         self.transform = transform
         self.length = length
+        self.device = device
         self.n_frames = 50
         self.anno_path = '../Crash-1500.txt'
         self.toa_dict = self.get_toa_all(self.anno_path)
@@ -70,36 +73,43 @@ class MyDataset(Dataset):
 
     def __getitem__(self, index):
         start = index
-        end = index + self.seq_length
-        print('Getting images from {} to {}'.format(start, end))
+        # end = index + self.seq_length
+        end = index + self.n_frames
+        # print('Getting images from {} to {}'.format(start, end))
         indices = list(range(start, end))
         images = []
         for i in indices:
             image_path = self.image_paths[i][0]
-            # print(image_path)
+            # print('image_path :', image_path)
             image = Image.open(image_path)
             if self.transform:
                 image = self.transform(image)
             images.append(image)
+        # print('=============================')
 
         #getting the video ID
         video_id = self.image_paths[i][0]
         vid = video_id.split('/')[-2]
 
-        x = torch.stack(images) #TODO: device
+        x = torch.stack(images).to(self.device)
         if self.image_paths[start][1] == 1:
-            label = 0
+            label = 0,1 #for positive class
+            try:
+                toa = [self.toa_dict[vid]]
+            except:
+                toa = [self.n_frames + 1]
         else:
-            label = 1
-        # y = torch.tensor([self.image_paths[start][1]], dtype=torch.long)
-        y = torch.tensor([label]) #TODO: device
-
-        if label == 1:
-            toa = [self.toa_dict[vid]]
-        else:
+            label = 1,0 #for negative class
             toa = [self.n_frames + 1]
-        print(toa)
-        toa = torch.Tensor((toa)) #TODO: device, probably datatype long also
+        # y = torch.tensor([self.image_paths[start][1]], dtype=torch.long)
+        y = torch.tensor([label], dtype=torch.float32).to(self.device)
+
+        # if label == (0,1):
+        #     print(self.toa_dict[vid])
+        #     toa = [self.toa_dict[vid]]
+        # else:
+        #     toa = [self.n_frames + 1]
+        toa = torch.Tensor((toa)).to(self.device)
 
         return x, y, toa
 
